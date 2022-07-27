baaa112014c2b1833ced5ca2d66af3f7647c22df
unknown <borgert180@gmail.com>
2022-07-27 13:18:42 -0300

    Aula sobre var, let

---
+++ Avançado/script.js
@@ -0,0 +1,17 @@
+//setInterval
+
+//function acao(){
+//document.write("Executando... <br/>");
+//};
+
+//setInterval(()=>{
+//    document.write("Executando... <br/>");
+//}, 1000); //Executa de 1 em 1 segundo
+
+//setTimeout(acao, 3000); //Depois de 3 segundo ele chama a ação
+
+//A Variavel let é como se fosse uma variavel exclusiva daquele bloco
+//if(true){
+//    let lata="k" Essa variavel pode ser utilizada dentro desse if
+//}
+//const é para criar uma variavel imutavel e eu não consigo inicializala sem passar o valor
