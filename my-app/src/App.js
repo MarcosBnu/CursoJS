51e04a24f25eb845a6c5421240fba563c2d78ea7
unknown <borgert180@gmail.com>
2022-07-29 13:32:48 -0300

    Iniciei o projeto, aprendi sobre o setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
 function App() {
+  const [aluno, setAluno] = useState('')
+  function Trocandonome(nome){
+    setAluno(nome);
+  }
+
+  const [idade, setIdade] = useState('')
+  function Trocandonome(idade){
+    setIdade(idade);
+  }
+
+  const [Email, setEmail] = useState('')
+  function Trocandonome(email){
+    setEmail(email);
+  }
+
+  function registrar(e){
+    e.preventDefault();
+    alert("Registrou")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bem vindo ao meu projeto</h1>
+      <h2><Nome aluno="Marcos"></Nome></h2>
+      <h2>Ola: {aluno}</h2>
+      <button onClick={ () => Trocandonome('nome aleatorio')}>
+        mudar nome
+      </button>
+      <br></br><br></br><br></br><br></br>
+
+      <form onSubmit={registrar}>
+        <label>Nome: </label>
+        <input 
+        value={aluno}
+        onChange={(e)=> setAluno(e.target.value)}
+        placeholder='Digite seu nome'></input>
+        
+        <br></br><br></br>
+
+        <label>Idade: </label>
+        <input
+        value={idade}
+        onChange={(e)=> setIdade(e.target.value)}
+        placeholder='Digite sua idade'></input>
+        <br></br><br></br>
+
+
+        <label>Email: </label>
+        <input 
+        value={Email}
+        onChange={(e)=> setEmail(e.target.value)}
+        placeholder='Digite seu email'></input>
+
+
+        <br></br><br></br>
+        <button type="submit">Registrar</button>
+      </form>
+      <br></br><br></br><br></br><br></br>
+      <h2>Ola: {aluno}, sua idade é: {idade} , e seu email é: {Email} </h2>
     </div>
   );
 }
 
 export default App;
+
+function Nome({aluno}){
+  return(
+    <span>Bem vindo {aluno}</span>
+  )
+}
